17bc5b21eb9e242c6d7f74d18d070584c1a00a6d
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-18 11:36:53 -0300

    chore: add config of openapi

---
+++ src/main/java/dev/java/ecommerce/basket_service/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package dev.java.ecommerce.basket_service.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Basket Service API")
+                        .version("1.0"));
+    }
+}
